7bcb14fc546457914f44265b662478fdea356d89
Gateway47 <itsregalo047@gmail.com>
2021-04-25 00:12:30 +0300

    server and client communication

--- socket_programming/client.py
+++ socket_programming/client.py
@@ -7,4 +7,18 @@ ADDRESS = (SERVER, PORT)
 disconnect_alert_message = "disconnected"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect()
+client.connect(ADDRESS)
+
+def send_message(the_message):
+    message = the_message.encode('utf-8')
+    message_length = len(message)
+    send_length = str(message_length).encode('utf-8')
+    send_length += b" " * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    
+send_message("Hello Gift")
+input()
+send_message("Save us master")
+input()
+send_message(disconnect_alert_message)
