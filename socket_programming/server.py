beba167abbe5e5b425a7607a311391e4fa0fde20
Gateway47 <itsregalo047@gmail.com>
2021-04-24 23:46:19 +0300

    server complete and working fine

--- socket_programming/server.py
+++ socket_programming/server.py
@@ -1,5 +1,37 @@
 import socket
 import threading
 
+HEADER = 64
 PORT = 8000
 SERVER = socket.gethostbyname(socket.gethostname())
+ADDRESS = (SERVER, PORT)
+disconnect_alert_message = "disconnected"
+
+#create socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDRESS)
+
+def handle_client(connection, address):
+    print(f"New Connection {address} just connected")
+    connected = True
+    while connected:
+        message_length = connection.recv(HEADER).decode("utf-8")
+        message_length = int(message_length)
+        message = connection.recv(message_length).decode("utf-8")
+        
+        if message == disconnect_alert_message:
+            connected = False
+        
+        print(f"{address} - {message}")
+    connection.close()
+
+def start():
+    server.listen()
+    while True:
+        connection, address = server.accept()
+        thread = threading.Thread(target=handle_client, args=(connection, address))
+        thread.start()
+        print(f"Active connections: {threading.activeCount() - 1}")
+
+print("server starting")
+start()
